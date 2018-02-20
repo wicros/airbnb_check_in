79ada6e67e53defe954397b3726b1ec35ad3c96c
wicros824 <wicros.wang@metroengines.jp>
2018-02-20 19:40:56 +0900

    updateUI

--- app/src/main/java/jp/metroengines/metrocheckin/activity/MainActivity.java
+++ app/src/main/java/jp/metroengines/metrocheckin/activity/MainActivity.java
@@ -55,8 +55,8 @@ public class MainActivity extends BaseActivity {
 
     private void change_local(String local) {
         SPUtils.put(this, SPUtils.LOCAL, local);
-        //startActivity(new Intent(this, ReservationActivity.class));
-        startActivity(new Intent(this, FailureActivity.class));
+        startActivity(new Intent(this, ReservationActivity.class));
+        //startActivity(new Intent(this, FailureActivity.class));
         //startActivity(new Intent(this, SuccessActivity.class));
         //startActivity(new Intent(this, GuestInfoActivity.class));
     }
